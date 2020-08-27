ebaa2ef1911ee945dd97a260dc6c50531dfc3a18
Fakeha Rahman <fakeha126@gmail.com>
2020-08-27 16:26:41 +0530

    express module

--- express/app.js
+++ express/app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const path = require("path");
 
 const bodyParser = require("body-parser");
 const express = require("express");
@@ -6,13 +7,15 @@ const express = require("express");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(adminRoutes);
+app.use(express.static(path.join(__dirname, "public")))
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found :(</h1>");
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 });
 
 app.listen(8080);
--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,14 +1,17 @@
+const path = require("path");
 const express = require("express");
 
+const rootDir= require('../util/path')
+
 const router = express.Router();
+// /admin/add-product=>GET
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
-  );
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
-router.post("/product", (req, res, next) => {
+// /admin/add-product =>POST
+router.post("/add-product", (req, res, next) => {
   console.log(req.body, req.body.title);
   res.redirect("/");
 });
 
-module.exports=router;
+module.exports = router;
