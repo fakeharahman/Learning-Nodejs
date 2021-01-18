3faecef09d3de9d61acc2fa761c321a1eea94954
fakeharahman <fakeha126@gmail.com>
2021-01-18 15:20:14 +0530

    images and errors

--- Rest API/routes/feed.js
+++ Rest API/routes/feed.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const { check } = require("express-validator");
+const { body } = require("express-validator");
 
 const feedController = require("../controllers/feed");
 
 const router = express.Router();
 
-router.get("/posts", [check("title").isLength({min: 5}), check("content").isLength({min: 5})] ,feedController.getFeed);
-router.post("/post", feedController.postPost);
+router.get("/posts", feedController.getFeed);
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 7 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.postPost
+);
+router.get('/post/:id', feedController.getPost)
 
 module.exports = router;
